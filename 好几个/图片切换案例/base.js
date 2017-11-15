83826165e25a50804f87a46eb61be83eed37b9d0
cuiweiqiang <1074366253@qq.com>
2017-11-16 00:03:12 +0800

    举例子

---
+++ 好几个/图片切换案例/base.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Administrator on 2017/8/28.
+ */
+/**
+ * 根据id获取元素
+ * @param idName 获取id名
+ * @returns {Element}
+ */
+function id(idName){
+    return document.getElementById(idName);
+}
